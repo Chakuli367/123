72cba875dbb65350043f0c6d6a66c8c22d927da5
Chakuli367 <mygoalgrid@gmail.com>
2025-05-14 07:17:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, jsonify, request
 import requests
-
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
+
+
 
 OLLAMA_URL = "http://localhost:11434"
 MODEL_NAME = "llama3"
